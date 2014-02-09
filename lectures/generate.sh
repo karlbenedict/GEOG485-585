5960ccbec020b2f3ea6ace30fdd86fbe0f7633a7
Karl Benedict <kbene@unm.edu>
2014-02-09 13:46:25 -0700

    Finalized week 4 lecture materials

--- lectures/generate.sh
+++ lectures/generate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-find . -iname "*.md" -exec /usr/local/bin/pandoc --slide-level 3 -c slides.css --self-contained --standalone -t dzslides \{\} -o \{\}.slides.html \;
-find . -iname "*.md" -exec /usr/local/bin/pandoc --slide-level 3 -c slides.css --standalone -t dzslides \{\} -o \{\}.slides.linked.html \;
+find . -iname "*.md" -exec /usr/local/bin/pandoc --slide-level 3 -c slides.css -c slidesSpecific.css --self-contained --standalone -t dzslides \{\} -o \{\}.slides.html \;
+find . -iname "*.md" -exec /usr/local/bin/pandoc --slide-level 3 -c slides.css -c slidesSpecific.css --standalone -t dzslides \{\} -o \{\}.slides.linked.html \;
 find . -iname "*.md" -exec /usr/local/bin/pandoc --toc -c ../page.css --self-contained --standalone \{\} -o \{\}.html \;
 find . -iname "*.md" -exec /usr/local/bin/pandoc --toc -c ../page.css --standalone \{\} -o \{\}.linked.html \;
 find . -iname "*.md" -exec /usr/local/bin/pandoc --toc --template ~/Dropbox/Active/Repos/Pandoc\ Templates/default_endFloat.latex --latex-engine=xelatex --self-contained --standalone \{\} -o \{\}.pdf \;
