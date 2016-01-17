4e073ba6fcea272f6c6ca8312e647485980296dd
Karl Benedict <kbene@unm.edu>
2016-01-16 17:54:05 -0700

    removed legacy files and folders

--- milestones/generate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-/usr/local/bin/pandoc --toc -c ../page.css --self-contained --standalone milestones.md -o milestones.html
-
-/usr/local/bin/pandoc --toc -c ../page.css --standalone milestones.md -o milestones.linked.html
-
-/usr/local/bin/pandoc --template ~/Dropbox/Active/Repos/Pandoc\ Templates/default_endFloat.latex --self-contained --standalone milestones.md -o milestones.pdf
