83370f5a5cf425d257ed5ebf7d3a8fe0f6b48b51
Karl Benedict <kbene@unm.edu>
2016-01-30 09:50:26 -0700

    removed cruft

--- deep-dives/generate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-/usr/local/bin/pandoc --toc -c ../page.css --self-contained --standalone deep-dives.md -o deep-dives.html
-
-/usr/local/bin/pandoc --toc -c ../page.css --standalone deep-dives.md -o deep-dives.linked.html
-
-/usr/local/bin/pandoc --toc --latex-engine=xelatex --template ~/Dropbox/Active/Repos/Pandoc\ Templates/default_endFloat.latex --self-contained --standalone deep-dives.md -o deep-dives.pdf
--- goalsAndObjectives/generate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-/usr/local/bin/pandoc --toc -c ../page.css --self-contained --standalone goalsAndObjectives.md -o goalsAndObjectives.html
-
-/usr/local/bin/pandoc --toc -c ../page.css --standalone goalsAndObjectives.md -o goalsAndObjectives.linked.html
-
-/usr/local/bin/pandoc --toc --latex-engine=xelatex --template ~/Dropbox/Active/Repos/Pandoc\ Templates/default_endFloat.latex --self-contained --standalone goalsAndObjectives.md -o goalsAndObjectives.pdf
