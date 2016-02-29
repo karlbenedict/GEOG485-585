b3b2619cee3a22201bbada856dac7ee7a6e45923
Karl Benedict <kbene@unm.edu>
2016-02-28 19:53:37 -0700

    added `--filter pandoc-fignos` filter to the PDF and HTML generation commands to enable support for cross-reference linking for figures by number based on markdown tagging.

--- generate_weekly.sh
+++ generate_weekly.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-/usr/local/bin/pandoc --toc -c page.css --self-contained --standalone week*.md -o week_all.html
+/usr/local/bin/pandoc --toc --filter pandoc-fignos -c page.css --self-contained --standalone week*.md -o week_all.html
 
-/usr/local/bin/pandoc --toc -c page.css --standalone week*.md -o week_all.linked.html
+/usr/local/bin/pandoc --filter pandoc-fignos --toc -c page.css --standalone week*.md -o week_all.linked.html
 
-/usr/local/bin/pandoc --toc --chapters --template default.latex --self-contained --standalone week*.md -o week_all.pdf
-/usr/local/bin/pandoc --toc --chapters --template default.latex --self-contained --standalone week*.md -o week_all.tex
+/usr/local/bin/pandoc --toc --filter pandoc-fignos --chapters --template default.latex --self-contained --standalone week*.md -o week_all.pdf
+/usr/local/bin/pandoc --toc --filter pandoc-fignos --chapters --template default.latex --self-contained --standalone week*.md -o week_all.tex
 
