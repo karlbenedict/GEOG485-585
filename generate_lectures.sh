e7026038bd66f2c38eff5c5ade147e6f60dab175
Karl Benedict <kbene@unm.edu>
2016-02-28 21:50:03 -0700

    Added Week 7 lecture materials and regenerated the associated products

--- generate_lectures.sh
+++ generate_lectures.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-/usr/local/bin/pandoc --toc -c page.css --self-contained --standalone lecture*.md -o lecture_all.html
+/usr/local/bin/pandoc --toc -c page.css --filter pandoc-fignos --self-contained --standalone lecture*.md -o lecture_all.html
 
-/usr/local/bin/pandoc --toc -c page.css --standalone lecture*.md -o lecture_all.linked.html
+/usr/local/bin/pandoc --toc -c page.css --filter pandoc-fignos --standalone lecture*.md -o lecture_all.linked.html
 
-/usr/local/bin/pandoc --slide-level 3 -c slides.css -c slide_specific.css --self-contained --standalone -t dzslides lecture*.md -o lecture_all.slides.html
+/usr/local/bin/pandoc --slide-level 3 --filter pandoc-fignos -c slides.css -c slide_specific.css --self-contained --standalone -t dzslides lecture*.md -o lecture_all.slides.html
 
-/usr/local/bin/pandoc --slide-level 3 -c slides.css -c slide_specific.css --standalone -t dzslides lecture*.md -o lecture_all.slides.linked.html
+/usr/local/bin/pandoc --slide-level 3 --filter pandoc-fignos -c slides.css -c slide_specific.css --standalone -t dzslides lecture*.md -o lecture_all.slides.linked.html
 
-/usr/local/bin/pandoc --toc --chapters --template default.latex --self-contained --standalone --latex-engine=xelatex lecture*.md -o lecture_all.pdf
+/usr/local/bin/pandoc --toc --filter pandoc-fignos --chapters --template default.latex --self-contained --standalone --latex-engine=xelatex lecture*.md -o lecture_all.pdf
 
