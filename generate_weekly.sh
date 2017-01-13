ee3d5ea87594f0451417db9b05aa1139fb50225f
Karl Benedict <kbene@unm.edu>
2017-01-13 10:23:38 -0700

    regenerated weekly HTML files as HTML5

--- generate_weekly.sh
+++ generate_weekly.sh
@@ -7,14 +7,14 @@
 #/usr/local/bin/pandoc --toc --filter pandoc-fignos --chapters --template default.latex --self-contained --standalone week*.md -o week_all.pdf
 #/usr/local/bin/pandoc --toc --filter pandoc-fignos --chapters --template default.latex --self-contained --standalone week*.md -o week_all.tex
 
-find . -name "week*.md" -exec /usr/local/bin/pandoc -c page.css --standalone '{}'  -o '{}'.html \;
+find . -name "week*.md" -exec /usr/local/bin/pandoc -c page.css --to=html5 --standalone '{}'  -o '{}'.html \;
 
 find . -name "week*.md" -exec /usr/local/bin/pandoc  --latex-engine=xelatex '{}'  -o '{}'.pdf \;
 
 
-/usr/local/bin/pandoc --toc  -c page.css --self-contained --standalone week*.md -o week_all.html
+/usr/local/bin/pandoc --toc  -c page.css  --to=html5 --self-contained --standalone week*.md -o week_all.html
 
-/usr/local/bin/pandoc --toc -c page.css --standalone week*.md -o week_all.linked.html
+/usr/local/bin/pandoc --toc -c page.css  --to=html5 --standalone week*.md -o week_all.linked.html
 
 /usr/local/bin/pandoc --toc  --chapters --template default.latex --self-contained --standalone week*.md -o week_all.pdf --latex-engine=xelatex
 /usr/local/bin/pandoc --toc  --chapters --template default.latex --self-contained --standalone week*.md -o week_all.tex --latex-engine=xelatex
